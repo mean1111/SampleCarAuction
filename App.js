e8075b87553d7d18e715685483a3b1e3b3f613b7
mean1111 <mean1111@gmail.com>
2019-01-08 17:37:37 +0900

    make LoginScreen

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text,StyleSheet, View } from 'react-native';
+import LoginScreen from './screens/LoginScreen';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View >
+        <LoginScreen/>
       </View>
     );
   }
